f6486124c55a97b80fb8581b981b00515a1e13b6
kevtr0n <kevphayden@gmail.com>
2023-02-07 16:06:51 -0500

    updates

---
+++ labs/logger/logger.py
@@ -0,0 +1,26 @@
+# import your libraries here.
+from datetime import datetime
+
+log_file = "log-file.log"
+
+def log(text, log_file=log_file):
+    '''
+    @text the text that you want to enter into the log.
+    @log_file the full path to the log file.
+    '''
+
+    # open up the log file in the correct mode.
+    # create a string that has the current date and time in the beginning of the text.
+    # append the text to the end of the file.
+    # close the file.
+    return None
+
+
+def dump_logs(log_file=log_file):
+    '''
+    This function prints out
+    '''
+    # open up the log file in the correct mode.
+    # read the file into a list.
+    # iterate through each item in the list and print out the results.
+    return None
