0159202304f269f6dcc603160286d58bb927fb5e
Kevin Hayden <Kevin.Hayden@ibm.com>
2023-02-23 16:13:36 -0500

    adding lab 5

--- labs/conditionals/conditionals.py
+++ labs/conditionals/conditionals.py
@@ -1,5 +1,13 @@
 '''
-Lab 5 - Conditionals and Decision Trees
+Assignment #5:
+
+    Tests the various capabilities of the functions  
+    defined in the conditionals.py file.
+
+
+@author Kevin Hayden
+@date 2023-02-11
+@org Marist College - Department of Computing Science
 
 Syntax for single decision:
 
@@ -20,5 +28,12 @@ if <condition> :
 elif <condition> :
     <body>
 else:
+    <body>
+    
+Syntax for try/catch:
+
+try:
+    <body>
+except <exception-type> :
     <body>
 '''
--- labs/conditionals/main.py
+++ labs/conditionals/main.py
@@ -1,5 +1,13 @@
 '''
+Assignment #5:
 
+    Tests the various capabilities of the functions  
+    defined in the conditionals.py file.
+
+
+@author Kevin Hayden
+@date 2023-02-11
+@org Marist College - Department of Computing Science
 '''
 
 from conditionals import *
@@ -7,8 +15,6 @@ from conditionals import *
 
 def main():
     print("Test code goes here")
-    grade = calculate_letter_grade([-30])
-    print(grade)
 
 
 main()
